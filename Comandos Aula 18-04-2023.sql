6768ce81dfff0c1f6895fa8b193989624bf502af
gabrielcaproni <gabrielcaproni09@gmail.com>
2023-04-18 10:52:00 -0300

    Atualizado em 18/04/202

--- Comandos Aula 18-04-2023.sql
+++ Comandos Aula 18-04-2023.sql
@@ -253,3 +253,27 @@ COMMIT;
 
 SELECT * FROM cliente;
 editora
+
+#                       TRIGGERS                      #
+
+
+#Proposta: Criar um trigger (tri_logApagaContaVinculada) que registre na Auditoria a exclusão de uma conta vinculada
+
+delimiter $
+CREATE TRIGGER tri_LogApagaContaVinculada
+AFTER DELETE 
+ON contavinculada
+FOR EACH ROW 
+BEGIN 
+	SELECT nome INTO @nomeCliente FROM cliente WHERE idCliente = OLD.CLIENTE_idCliente;
+	SET @mensagem = CONCAT("Exclusão de conta do cliente: ", @nomeCliente);
+
+	INSERT INTO auditoria VALUES(NULL, @mensagem, "contavinculada", NOW(), USER());
+END$
+delimiter;
+
+
+
+
+
+
